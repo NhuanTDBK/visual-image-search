a47f2a9fde976893f7fdea42f9bde24d7aceabe5
Nhuan Tran <nhuan.tran@onemount.com>
2021-06-24 13:21:50 +0700

    Add baseline

--- models/object_detection.py
+++ models/object_detection.py
@@ -1,23 +1,18 @@
-from typing import Dict
-
+import numpy as np
 import tensorflow as tf
 
 
-# def load_img(path: str):
-#    contents = tf.io.read_file(path)
-#    h, w = tf.image.extract_jpeg_shape(contents)
-#    img = tf.image.decode_jpeg(contents, channels=3)
-#    img, im_height, im_width = load_img(path)
-#    converted_img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
-#    return img, h, w
+def _to_int32(a):
+    return np.int32(np.ceil(a))
 
 
 def extract_patches(detector: tf.keras.models.Model,
                     img: tf.TensorArray,
                     min_score: float = 0.4,
-                    max_boxes: int = 10) -> Dict:
-    im_height, im_width = tf.shape(img)
-    result = detector(img)
+                    max_boxes: int = 10):
+    shape = tf.shape(img)
+    im_height, im_width = shape[0].numpy(), shape[1].numpy()
+    result = detector(tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...])
 
     result = {key: value.numpy() for key, value in result.items()}
 
@@ -32,16 +27,16 @@ def extract_patches(detector: tf.keras.models.Model,
             example = {}
             ymin, xmin, ymax, xmax = tuple(boxes[i])
             class_name = entities[i].decode("ascii")
-            xmin, xmax, ymin, ymax = (xmin * im_width, xmax * im_width, ymin * im_height, ymax * im_height)
+
+            xmin, xmax, ymin, ymax = _to_int32(xmin * im_width), _to_int32(xmax * im_width), _to_int32(
+                ymin * im_height), _to_int32(ymax * im_height)
             tmp = tf.image.crop_to_bounding_box(img, ymin, xmin, ymax - ymin, xmax - xmin)
 
             example["class_name"] = class_name
             example["arr"] = tmp.numpy()
+            example["score"] = scores[i]
 
-            example["xmin"] = xmin
-            example["xmax"] = xmax
-            example["ymin"] = ymin
-            example["ymax"] = ymax
+            example["bounding_box"] = (xmin, xmax, ymin, ymax)
 
             examples.append(example)
 
